21eedf8046b97f57d21b358f9a420571ba91a90e
karantabrizi <karan.tabrizi@photoninfotech.net>
2015-06-03 10:20:28 -0500

    initialization

---
+++ JCP iOS/JCP iOS/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  JCP iOS
+//
+//  Created by karan tabrizi on 6/3/15.
+//  Copyright (c) 2015 karan tabrizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
---
+++ JCP iOS/JCP iOSTests/JCP_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  JCP_iOSTests.swift
+//  JCP iOSTests
+//
+//  Created by karan tabrizi on 6/3/15.
+//  Copyright (c) 2015 karan tabrizi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JCP_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
